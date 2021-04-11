62680f5916c9a433b62ca16af3c9db931673f79c
David Négrier <d.negrier@thecodingmachine.com>
2021-04-11 23:15:32 +0200

    Adding Typescript scripting
    This sets up Typescript scripting.
    As a sideeffect, this also adds a local webserver that can be used to develop maps locally.

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+/// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
+
+console.log('Script started successfully');
+WA.openCoWebSite('https://workadventu.re');
