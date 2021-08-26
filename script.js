dd3fc9c8b98c5a58aca177055a36579732b21e12
moufmouf <d.negrier@thecodingmachine.com>
2021-08-26 14:53:27 +0000

    Deploying to gh-pages from master @ 7cf5eb3ea808f7dd928913e21e570747932dd21a 🚀

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-(()=>{"use strict";let o;const e=new Date,n=e.getHours()+":"+e.getMinutes()+":"+e.getSeconds();WA.onEnterZone("clock",(()=>{o=WA.openPopup("clockPopup","It's "+n,[])})),WA.onLeaveZone("clock",(function(){void 0!==o&&(o.close(),o=void 0)}))})();
+(()=>{"use strict";let o;const e=new Date,n=e.getHours()+":"+e.getMinutes();WA.room.onEnterZone("clock",(()=>{o=WA.ui.openPopup("clockPopup","It's "+n,[])})),WA.room.onLeaveZone("clock",(function(){void 0!==o&&(o.close(),o=void 0)}))})();
 //# sourceMappingURL=script.js.map
