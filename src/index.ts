a44703e6d661c4d48af49f9427fd025f7ec9af08
Valdo Romao <v.romao@thecodingmachine.com>
2022-03-03 14:27:09 +0100

    Updated script to add tag logs

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,28 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
-import {bootstrapExtra} from "@workadventure/scripting-api-extra";
-
-// The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
-bootstrapExtra().catch(e => console.error(e));
+console.log('Script started successfully');
 
 let currentPopup: any = undefined;
 
-WA.room.onEnterLayer('clockZone').subscribe(() => {
+// Waiting for the API to be ready
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags)
+    
+    WA.room.onEnterLayer('clockZone').subscribe(() => {
     const today = new Date();
     const time = today.getHours() + ":" + today.getMinutes();
     currentPopup = WA.ui.openPopup("clockPopup","It's " + time,[]);
 })
 
 WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
+
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+}).catch(e => console.error(e));
 
 function closePopUp(){
     if (currentPopup !== undefined) {
