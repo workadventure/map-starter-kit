c6a834c920cabb1fd9159d20ff1d64b56d36af55
Valdo <vteixeiraromao@gmail.com>
2023-01-02 11:16:57 +0100

    Fixed clock zone and script path

--- src/main.ts
+++ src/main.ts
@@ -11,13 +11,13 @@ WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
-    WA.room.onEnterLayer('clockZone').subscribe(() => {
+    WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
         const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup","It's " + time,[]);
+        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
     })
 
-    WA.room.onLeaveLayer('clockZone').subscribe(closePopup)
+    WA.room.area.onLeave('clock').subscribe(closePopup)
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
