5943534e173dba13c451e713b79190cd97dbd1c8
Alexis Faizeau <a.faizeau@workadventu.re>
2023-09-28 14:51:44 +0200

    Implement map optimization by the .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
+import 'dotenv/config';
 import { defineConfig } from "vite";
-import { getMaps, getMapsOptimizers, getMapsScripts } from "wa-map-optimizer-vite";
+import { getMaps, getMapsOptimizers, getMapsScripts, LogLevel, OptimizeOptions } from "wa-map-optimizer-vite";
 
 const maps = getMaps();
+
+let optimizerOptions: OptimizeOptions = {
+    logs: process.env.LOG_LEVEL && process.env.LOG_LEVEL in LogLevel ? LogLevel[process.env.LOG_LEVEL] : LogLevel.NORMAL,
+};
+
+if (process.env.TILESET_OPTIMIZATION && process.env.TILESET_OPTIMIZATION === "true") {
+    const qualityMin = process.env.TILESET_OPTIMIZATION_QUALITY_MIN ? parseInt(process.env.TILESET_OPTIMIZATION_QUALITY_MIN) : 0.9;
+    const qualityMax = process.env.TILESET_OPTIMIZATION_QUALITY_MAX ? parseInt(process.env.TILESET_OPTIMIZATION_QUALITY_MAX) : 1;
+
+    optimizerOptions.output = {
+        tileset: {
+            compress: {
+                quality: [qualityMin, qualityMax],
+            }
+        }
+    }
+}
 
 export default defineConfig({
     base: "./",
@@ -13,7 +31,7 @@ export default defineConfig({
             },
         },
     },
-    plugins: [...getMapsOptimizers(maps)],
+    plugins: [...getMapsOptimizers(maps, optimizerOptions)],
     server: {
         host: "localhost",
         headers: {
