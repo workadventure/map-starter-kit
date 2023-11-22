06ae9d95e08cf1c38766bcf86556271ae9710167
neosoftgroupe <136612587+neosoftgroupe@users.noreply.github.com>
2023-11-22 11:03:09 +0100

    Ajout map et tilesets

---
+++ tilesets/23_Room_Builder_32x32_edited.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="23_Room_Builder_32x32_edited" tilewidth="32" tileheight="32" tilecount="5609" columns="79">
+ <image source="../../nior-recrutement-map/tilesets/23_Room_Builder_32x32_edited.png" width="2550" height="2278"/>
+</tileset>
