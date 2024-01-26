482ae6bdf827d1911e9e0d8a2a3391cbf061c24b
neosoftgroupe <136612587+neosoftgroupe@users.noreply.github.com>
2024-01-26 16:05:23 +0100

    Ajout modifications
    - Date

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,78 @@ let currentPopup: any = undefined;
 
 // Waiting for the API to be ready
 
+
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags)
+
+    function verifierHeure() {
+        // Obtenir l'heure actuelle
+        var maintenant = new Date();
+      
+        // Définir l'heure à laquelle tu souhaites déclencher l'action
+        var heureAction1 = new Date();
+        heureAction1.setHours(15); // Heure : 0 (exemple)
+        heureAction1.setMinutes(35); // Minutes : 1 (exemple)
+        heureAction1.setSeconds(0); // Secondes : 0 (exemple)
+
+        var heureAction2 = new Date();
+        heureAction2.setHours(12); // Heure : 0 (exemple)
+        heureAction2.setMinutes(45); // Minutes : 0 (exemple)
+        heureAction2.setSeconds(0); // Secondes : 0 (exemple)
+      
+        // Vérifier si l'heure actuelle correspond à l'heure d'action
+        if (maintenant.getTime() === heureAction1.getTime()) {
+          // Déclencher ton action ici
+          //console.log("Action déclenchée à l'heure précise !");
+          //WA.chat.sendChatMessage("Action déclenchée à l'heure précise !", " Mr Robot");
+          WA.ui.banner.openBanner({
+            id: "banner-test",
+            text: "On va bientôt commencer, rendez-vous dans l'amphi ! :)",
+            bgColor: "#0055FF",
+            textColor: "#FFFFFF",
+            closable: false
+        });
+        } else if (maintenant.getTime() === heureAction2.getTime()) {
+            // Déclencher ton action ici
+            //console.log("Action déclenchée à l'heure précise !");
+            //WA.chat.sendChatMessage("Action déclenchée à l'heure précise !", " Mr Robot");
+            WA.ui.banner.openBanner({
+              id: "banner-test",
+              text: "C'est bientôt la fin... Rendez-vous dans l'amphi pour la conclusion !",
+              bgColor: "#0055FF",
+              textColor: "#FFFFFF",
+              closable: false
+          });
+          } //else {
+            // Aucune des heures n'est encore passée
+            //console.log("Aucune des heures n'est encore passée.");
+        //}
+    }
+      // Vérifier l'heure toutes les secondes
+      setInterval(verifierHeure, 1000);
+
+
+
+  /*  WA.room.area.onEnter('clock').subscribe(() => {
+        const today = new Date();
+        const time = today.getHours() + ":" + today.getMinutes();
+        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+    })
+
+    WA.room.area.onLeave('clock').subscribe(closePopup) */
+
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+
+
+
+
+}).catch(e => console.error(e));
+
+
 // Message qui s'affiche sur le chat à droite avec le lien du tuto (solution 2)
 //WA.chat.sendChatMessage('Bonjour ! Bienvenue à NIORT voici le tutoriel : https://landing.neosoft.fr/discord-0');
 
@@ -24,30 +96,15 @@ WA.room.area.onEnter('supportrh').subscribe(() => {
     }]);
 })
 
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
 
-    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
 
-}).catch(e => console.error(e));
-
-function closePopup(){
+/*function closePopup(){
     if (currentPopup !== undefined) {
         currentPopup.close();
         currentPopup = undefined;
     }
-}
+}**/
 
 export {};
