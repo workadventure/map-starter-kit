ec67f465929471350c32f43f494488b10e1a35be
Lucie MENAGE <l.menage@NeoSoft.local>
2024-02-15 11:57:36 +0100

    added auto navigate to move player to a specific coordinate

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ WA.onInit().then(() => {
     function verifierHeure() {
         // Obtenir l'heure actuelle
         var maintenant = new Date();
-      
+
         // Définir l'heure à laquelle tu souhaites déclencher l'action
         var heureAction1 = new Date();
         heureAction1.setHours(15); // Heure : 0 (exemple)
@@ -29,54 +29,54 @@ WA.onInit().then(() => {
         heureAction2.setHours(12); // Heure : 0 (exemple)
         heureAction2.setMinutes(45); // Minutes : 0 (exemple)
         heureAction2.setSeconds(0); // Secondes : 0 (exemple)
-      
+
         // Vérifier si l'heure actuelle correspond à l'heure d'action
         if (maintenant.getTime() === heureAction1.getTime()) {
-          // Déclencher ton action ici
-          //console.log("Action déclenchée à l'heure précise !");
-          //WA.chat.sendChatMessage("Action déclenchée à l'heure précise !", " Mr Robot");
-          WA.ui.banner.openBanner({
-            id: "banner-test",
-            text: "On va bientôt commencer, rendez-vous dans l'amphi ! :)",
-            bgColor: "#0055FF",
-            textColor: "#FFFFFF",
-            closable: false
-        });
+            // Déclencher ton action ici
+            //console.log("Action déclenchée à l'heure précise !");
+            //WA.chat.sendChatMessage("Action déclenchée à l'heure précise !", " Mr Robot");
+            WA.ui.banner.openBanner({
+                id: "banner-test",
+                text: "On va bientôt commencer, rendez-vous dans l'amphi ! :)",
+                bgColor: "#0055FF",
+                textColor: "#FFFFFF",
+                closable: false
+            });
         } else if (maintenant.getTime() === heureAction2.getTime()) {
             // Déclencher ton action ici
             //console.log("Action déclenchée à l'heure précise !");
             //WA.chat.sendChatMessage("Action déclenchée à l'heure précise !", " Mr Robot");
             WA.ui.banner.openBanner({
-              id: "banner-test",
-              text: "C'est bientôt la fin... Rendez-vous dans l'amphi pour la conclusion !",
-              bgColor: "#0055FF",
-              textColor: "#FFFFFF",
-              closable: false
-          });
-          } //else {
-            // Aucune des heures n'est encore passée
-            //console.log("Aucune des heures n'est encore passée.");
+                id: "banner-test",
+                text: "C'est bientôt la fin... Rendez-vous dans l'amphi pour la conclusion !",
+                bgColor: "#0055FF",
+                textColor: "#FFFFFF",
+                closable: false
+            });
+        } //else {
+        // Aucune des heures n'est encore passée
+        //console.log("Aucune des heures n'est encore passée.");
         //}
     }
-      // Vérifier l'heure toutes les secondes
-      setInterval(verifierHeure, 1000);
+    // Vérifier l'heure toutes les secondes
+    setInterval(verifierHeure, 1000);
 
-      // Initialisation autorisation des salles
-      WA.player.state.saveVariable("authorizedRooms", [1])
-      WA.player.state.saveVariable("quests", [])
-      WA.ui.modal.openModal({
+    // Initialisation autorisation des salles
+    WA.player.state.saveVariable("authorizedRooms", [1])
+    WA.player.state.saveVariable("quests", [])
+    WA.ui.modal.openModal({
         title: 'tuto',// mandatory, title of the iframe modal.
         src: "https://landing.neosoft.fr/discord-0", // mandatory, url of the iframe modal.
         position: "center"
-      })
-
-  /*  WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
     })
 
-    WA.room.area.onLeave('clock').subscribe(closePopup) */
+    /*  WA.room.area.onEnter('clock').subscribe(() => {
+          const today = new Date();
+          const time = today.getHours() + ":" + today.getMinutes();
+          currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+      })
+  
+      WA.room.area.onLeave('clock').subscribe(closePopup) */
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
@@ -89,7 +89,8 @@ WA.onInit().then(() => {
 // Message qui s'affiche sur le chat à droite avec le lien du tuto (solution 2)
 //WA.chat.sendChatMessage('Bonjour ! Bienvenue à NIORT voici le tutoriel : https://landing.neosoft.fr/discord-0');
 
-WA.room.area.onEnter('supportrh').subscribe(() => {
+WA.room.area.onEnter('supportrh').subscribe(async () => {
+
     // const today = new Date();
     // const time = today.getHours() + ":" + today.getMinutes();
 
@@ -98,9 +99,17 @@ WA.room.area.onEnter('supportrh').subscribe(() => {
         className: "primary",
         callback: (popup) => {
             popup.close();
+            WA.player.moveTo(321, 683, 10).then((result) => {
+                // console.log("res", !result.cancelled)
+                if (result) {
+                    WA.player.moveTo(695, 1255, 10);
+                }
+            });
         }
     }]);
 })
+
+// setInterval(async () => { console.log("position :", await WA.player.getPosition()) }, 1000)
 
 /*function closePopup(){
     if (currentPopup !== undefined) {
@@ -109,4 +118,4 @@ WA.room.area.onEnter('supportrh').subscribe(() => {
     }
 }**/
 
-export {};
+export { };
