08272605fc3f22af763b83857e21834b3c39d248
emilenascivet <emile.nascivet@neosoft.fr>
2024-02-27 10:31:12 +0100

    feat :  ajout des pages hubspot

--- src/gates/gate.ts
+++ src/gates/gate.ts
@@ -5,17 +5,17 @@ import { AREA } from "../constantes"
 // 3) Tout est fermé sauf porte 2 et 3...
 // 4) Tout est ouvert (calque collisions)
 
-WA.room.area.onEnter(AREA.FLOOR_LAYER.SUPPORT_RH_POPUP).subscribe(() => {
+WA.room.area.onEnter(AREA.FLOOR_LAYER.VIDEO_AGENCY).subscribe(() => {
     if ((WA.player.state.authorizedRooms as number[]).includes(1) 
     && !(WA.player.state.authorizedRooms as number[]).includes(2)) {
         WA.player.state.saveVariable("authorizedRooms", [1, 2])
         WA.room.hideLayer(AREA.DOORS_LAYER.ALL_DOORS_CLOSED)
         WA.room.showLayer(AREA.DOORS_LAYER.DOOR_CAREER_AREA)
     }
-    console.log('Player: ', WA.player.state.authorizedRooms)
 })
 
 WA.room.area.onEnter(AREA.FLOOR_LAYER.CAREER_AREA).subscribe(() => {
+
     if ((WA.player.state.authorizedRooms as number[]).includes(1)
     && (WA.player.state.authorizedRooms as number[]).includes(2)
     && !(WA.player.state.authorizedRooms as number[]).includes(3)) {
@@ -23,15 +23,16 @@ WA.room.area.onEnter(AREA.FLOOR_LAYER.CAREER_AREA).subscribe(() => {
         WA.room.hideLayer(AREA.DOORS_LAYER.DOOR_CAREER_AREA)
         WA.room.showLayer(AREA.DOORS_LAYER.DOOR_AGENCY_AREA)
     }
-    console.log('Player: ', WA.player.state.authorizedRooms)
 })
 
-WA.room.area.onEnter(AREA.FLOOR_LAYER.AGENCY_AREA).subscribe(() => {
+WA.room.area.onEnter(AREA.FLOOR_LAYER.AGENCY_AREA).subscribe(async () => {
+
     if ((WA.player.state.authorizedRooms as number[]).includes(1)
     && (WA.player.state.authorizedRooms as number[]).includes(2)
     && (WA.player.state.authorizedRooms as number[]).includes(3)
     && !(WA.player.state.authorizedRooms as number[]).includes(4)) {
         (WA.player.state.authorizedRooms as number[]).push(4)
+        WA.room.hideLayer(AREA.DOORS_LAYER.DOOR_AGENCY_AREA)
+        WA.room.showLayer(AREA.DOORS_LAYER.ALL_DOORS_OPENED)
     }
-    console.log('Player: ', WA.player.state.authorizedRooms)
 })
