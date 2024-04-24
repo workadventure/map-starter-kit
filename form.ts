d72d45cc9bfd3dd684a24a79f0af36b4e2a87ea3
janparacha <parmusta@gmail.com>
2024-04-24 17:19:23 +0200

    WIP V1 TP condition

--- form.ts
+++ form.ts
@@ -1,7 +1,6 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-console.log('Script started successfully');
-
+console.log("Script started successfully");
 
 const registrationArea = document.getElementById("registrationArea") as HTMLTextAreaElement;
 const saveButton = document.getElementById("inscrire") as HTMLButtonElement;
@@ -15,42 +14,43 @@ const gender = document.getElementById("gender") as HTMLInputElement;
 const searching = document.getElementById("searching") as HTMLInputElement;
 
 // Waiting for the API to be ready
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
+WA.onInit()
+  .then(() => {
+    console.log("Scripting API ready");
     console.log(WA.state.players);
-    
+
     saveButton.addEventListener("click", () => {
-        if (firstName.value === "" || lastName.value === "" || email.value === "") {
-            alert("Veuillez remplir les champs obligatoires");
-        } else {
-            alert("Inscription réussie");
-            WA.player.state.firstName = firstName.value;
-            WA.player.state.lastName = lastName.value;
-            WA.player.state.phone = phone.value;
-            WA.player.state.email = email.value;
-            WA.player.state.age = age.value;
-            WA.player.state.gender = gender.value;
-            WA.player.state.searching = searching.value;
-            WA.player.state.id = WA.state.loadVariable('indexPlayers');
+      if (firstName.value === "" || lastName.value === "" || email.value === "") {
+        alert("Veuillez remplir les champs obligatoires");
+      } else {
+        alert("Inscription réussie");
+        WA.player.state.firstName = firstName.value;
+        WA.player.state.lastName = lastName.value;
+        WA.player.state.phone = phone.value;
+        WA.player.state.email = email.value;
+        WA.player.state.age = age.value;
+        WA.player.state.gender = gender.value;
+        WA.player.state.searching = searching.value;
+        // WA.player.state.id = WA.state.loadVariable('indexPlayers');
 
-            WA.state.saveVariable('players', {
-                ...WA.state.loadVariable('players'),
-                [WA.state.loadVariable('indexPlayers')]: {
-                    firstName: WA.player.state.firstName,
-                    lastName: WA.player.state.lastName,
-                    age: WA.player.state.age,
-                    gender: WA.player.state.gender,
-                    searching: WA.player.state.searching,
-                    email: WA.player.state.email,
-                    phone: WA.player.state.phone,
-                }
-            })
-            WA.state.saveVariable('indexPlayers', WA.state.loadVariable('indexPlayers') + 1)
+        WA.state.saveVariable("players", {
+          ...WA.state.loadVariable("players"),
+          [WA.state.loadVariable("indexPlayers")]: {
+            firstName: WA.player.state.firstName,
+            lastName: WA.player.state.lastName,
+            age: WA.player.state.age,
+            gender: WA.player.state.gender,
+            searching: WA.player.state.searching,
+            email: WA.player.state.email,
+            phone: WA.player.state.phone,
+          },
+        });
+        WA.state.saveVariable("indexPlayers", WA.state.loadVariable("indexPlayers") + 1);
 
-            console.log(WA.state.players);
-        }
+        console.log(WA.state.players);
+      }
     });
-
-}).catch(e => console.error(e));
+  })
+  .catch((e) => console.error(e));
 
 export {};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import { log } from "console";
 
-console.log('Script started successfully');
+console.log("Script started successfully");
 
 let currentPopup: any = undefined;
 let formWebsite: any = undefined;
@@ -10,99 +11,115 @@ let formWebsite: any = undefined;
 let index: string = "azeaze";
 console.log("on vient de load !!!!!!!!!!!");
 
-WA.state.saveVariable('players', {
-    "0" :{
-        age: "29",
-        gender: "homme",
-        searching: "femme",
-        firstName: "Mark",
-        lastName: "Paul",
-        email: "email@gmail.com",
-        phone: "0601010101",
-    }
-})
+WA.onInit()
+  .then(() => {
+    WA.player.state.saveVariable("id", 2);
 
-WA.onInit().then(() => {
     WA.room.area.onEnter("registrationArea").subscribe(async () => {
-        console.log("Entering visibleNote layer");
+      console.log("Entering visibleNote layer");
 
-        formWebsite = await WA.ui.website.open({
-            url: "./form.html",
-            position: {
-                vertical: "top",
-                horizontal: "middle",
-            },
-            size: {
-                height: "60vh",
-                width: "50vw",
-            },
-            margin: {
-                top: "10vh",
-            },
-            allowApi: true,
-        });
+      formWebsite = await WA.ui.website.open({
+        url: "./form.html",
+        position: {
+          vertical: "top",
+          horizontal: "middle",
+        },
+        size: {
+          height: "60vh",
+          width: "50vw",
+        },
+        margin: {
+          top: "10vh",
+        },
+        allowApi: true,
+      });
+    });
+
+    WA.room.area.onEnter("to-date").subscribe(() => {
+      if (WA.state.loadVariable("index") === WA.player.state.loadVariable("id")) {
+        WA.nav.goToRoom("#from-queue");
+      }
     });
 
     WA.room.area.onLeave("registrationArea").subscribe(() => {
-        formWebsite.close();
-    })
-
-
-
+      formWebsite.close();
+    });
 
     //code Nicolas
-    WA.room.area.onEnter('showPlayer').subscribe(openPopup)
-    WA.room.area.onEnter('validatePlayer').subscribe(() => {
-        console.log(WA.state.players);
-        
-        const hasPlayers = Array.isArray(WA.state.players) && WA.state.players.length > 0;
-        // const isValidIndex = index >= 0 && index < WA.state.players.length;
-        
-        if (hasPlayers) {
-            const playerName = WA.state.players[WA.state.loadVariable('index')].firstName + WA.state.players[WA.state.loadVariable('index')].lastName;
-            WA.ui.openPopup("validatePlayerPopup", `${playerName}, on y va !`, []);
-        }
-    })
-    WA.room.area.onEnter('nextPlayer').subscribe(() => {
-        closePopup()
-        WA.state.saveVariable('index', WA.state.loadVariable('index') + 1)
-        openPopup()
-    })
+    WA.room.area.onEnter("showPlayer").subscribe(() => {
+      openPopup();
+      console.log(WA.state.loadVariable("index"), WA.player.state.loadVariable("id"));
+    });
 
-    WA.room.area.onLeave('showPlayer').subscribe(closePopup)
+    WA.room.area.onEnter("validatePlayer").subscribe(() => {
+      console.log(WA.state.players);
+
+      const hasPlayers =
+        typeof WA.state.loadVariable("players") === "object" && WA.state.loadVariable("index") in WA.state.players;
+
+      if (hasPlayers) {
+        const playerName =
+          WA.state.players[WA.state.loadVariable("index")].firstName +
+          WA.state.players[WA.state.loadVariable("index")].lastName;
+        WA.ui.openPopup("validatePlayerPopup", `${playerName}, on y va !`, []);
+      } else {
+        WA.ui.openPopup("validatePlayerPopup", "Il n'y a pas de prétendant(e)", []);
+      }
+    });
+
+    WA.room.area.onEnter("nextPlayer").subscribe(() => {
+      closePopup();
+      if (WA.state.loadVariable("index") in WA.state.players) {
+        WA.state.saveVariable("index", WA.state.loadVariable("index") + 1);
+      }
+      openPopup();
+    });
+
+    WA.room.area.onLeave("showPlayer").subscribe(closePopup);
     //fin code nicolas
 
+    bootstrapExtra()
+      .then(() => {
+        console.log("Scripting API Extra ready");
+      })
+      .catch((e) => console.error(e));
+  })
+  .catch((e) => console.error(e));
 
-
-    
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
-
-}).catch(e => console.error(e));
-
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
+function closePopup() {
+  if (currentPopup !== undefined) {
+    currentPopup.close();
+    currentPopup = undefined;
+  }
 }
 
 function openPopup() {
-    try {
-        currentPopup = WA.ui.openPopup("playersPopup", displayNotes(WA.state.loadVariable("players")[WA.state.loadVariable('index')]), []);
-    } catch (e) {
-        currentPopup = WA.ui.openPopup("playersPopup", "Il n'y a pas de prétendant(e)", []);
-    }
+  try {
+    currentPopup = WA.ui.openPopup(
+      "playersPopup",
+      displayNotes(WA.state.loadVariable("players")[WA.state.loadVariable("index")]),
+      []
+    );
+  } catch (e) {
+    currentPopup = WA.ui.openPopup("playersPopup", "Il n'y a pas de prétendant(e)", []);
+  }
 }
 
-function displayNotes(player: {firstName: string, lastName: string, age: string, gender: string, searching: string}){
-    return player.firstName + player.lastName + ", " + player.age + " ans\n" + player.gender.capitalize() + " cherche " + player.searching
+function displayNotes(player: { firstName: string; lastName: string; age: string; gender: string; searching: string }) {
+  return (
+    player.firstName +
+    player.lastName +
+    ", " +
+    player.age +
+    " ans\n" +
+    player.gender.capitalize() +
+    " cherche " +
+    player.searching
+  );
 }
 
-String.prototype.capitalize = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
+String.prototype.capitalize = function () {
+  return this.charAt(0).toUpperCase() + this.slice(1);
 };
-
 
 export {};
