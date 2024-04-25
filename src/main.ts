f5ff0dbbae8534085f3f5087a4f5b0c9423ce87b
Victor De Domenico <victordedomenico.du60@gmail.com>
2024-04-25 15:50:08 +0200

    Rebase master
    Rebase master

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,8 @@ let startTime = Date.now();
 let level: number = 1;
 
 // Waiting for the API to be ready
-WA.onInit().then(() => {
+WA.onInit()
+  .then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
     console.log('Player name: ', WA.player.name);
@@ -67,8 +68,87 @@ WA.onInit().then(() => {
         //Démarrer la partie
     })
 
-    WA.room.area.onLeave('clock').subscribe(() => closePopup(currentPopup));
+    WA.room.area.onEnter("processeurPopup").subscribe(async () => {
+      showPopup("Processeur", 
+      "Element central de votre ordinateur, responsable de l'execution des programmes et de la gestion des ressources de votre ordinateur.",
+       "processeur");
+    });
 
+    WA.room.area.onEnter("carteMerePopup").subscribe(async () => {
+      showPopup("Carte mère", 
+      "Elément central de votre ordinateur, responsable de la communication entre les différents composants de votre ordinateur.",
+       "carteMere");
+    });
+
+    WA.room.area.onEnter("ramPopup").subscribe(async () => {
+      showPopup("Mémoire vive", 
+      "Permet de stocker temporairement les données de vos programmes en cours d'execution.",
+       "ram");
+    });
+
+    WA.room.area.onEnter("carteGraphiquePopup").subscribe(async () => {
+      showPopup("Carte graphique", 
+      "Permet de traiter les informations graphiques de votre ordinateur.",
+       "carteGraphique");
+    });
+
+    WA.room.area.onEnter("ssdPopup").subscribe(async () => {
+      showPopup("SSD", 
+      "Permet de stocker de manière permanente vos données et programmes.",
+       "ssd");
+    });
+
+    WA.room.area.onEnter("disqueDurPopup").subscribe(async () => {
+      showPopup("Disque dur", 
+      "Permet de stocker de manière permanente vos données et programmes.",
+       "disqueDur");
+    });
+
+    WA.room.area.onEnter("ventiradPopup").subscribe(async () => {
+      showPopup("Ventirad", 
+      "Permet de refroidir votre processeur pour éviter la surchauffe.",
+       "ventirad");
+    });
+
+    WA.room.area.onEnter("alimentationPopup").subscribe(async () => {
+      showPopup("Alimentation", 
+      "Permet d'alimenter votre ordinateur en électricité.",
+       "alimentation");
+    });
+
+    WA.room.area.onLeave("processeurPopup").subscribe(async () => {
+      closePopup(currentPopup);
+    });
+
+    WA.room.area.onLeave("carteMerePopup").subscribe(async () => {
+      closePopup(currentPopup);
+    });
+
+    WA.room.area.onLeave("ramPopup").subscribe(async () => {
+      closePopup(currentPopup);
+    });
+
+    WA.room.area.onLeave("carteGraphiquePopup").subscribe(async () => {
+      closePopup(currentPopup);
+    });
+
+    WA.room.area.onLeave("ssdPopup").subscribe(async () => {
+      closePopup(currentPopup);
+    });
+
+    WA.room.area.onLeave("disqueDurPopup").subscribe(async () => {
+      closePopup(currentPopup);
+    });
+
+    WA.room.area.onLeave("ventiradPopup").subscribe(async () => {
+      closePopup(currentPopup);
+    });
+
+    WA.room.area.onLeave("alimentationPopup").subscribe(async () => {
+      closePopup(currentPopup);
+    });
+
+    
     WA.room.area.onEnter('timer').subscribe(() => {
         let count = 60;
         currentPopup = updatePopup(currentPopup, count);
@@ -82,7 +162,7 @@ WA.onInit().then(() => {
     });
 
     WA.room.area.onLeave('timer').subscribe(() => closePopup(currentPopup));
-
+    
     let podiumWebsite: any;
     let resultWebsite: any;
     let enterCounter = 0;
@@ -132,9 +212,7 @@ WA.onInit().then(() => {
     WA.room.onLeaveLayer("result").subscribe(() => {
         resultWebsite.close();
     });
-
-    WA.room.setTiles([{ x: 250, y: 250, tile: "test", layer: "podium" }]);
-
+    
     bootstrapExtra()
       .then(() => {
         console.log("Scripting API Extra ready");
