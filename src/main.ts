1deff85970a71f5dcebfceb0b7dc56189a1a70c5
ines-mgg <i.maganga@yahoo.com>
2024-04-25 21:20:18 +0200

    added new timer

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,16 @@ import game_data from "../game_data/game_data.json";
 import {
   addComponent,
   closePopup,
-  getTickets, startGame,
+  getTickets,
+  startGame,
   updatePopup,
-} from './functions';
+} from "./functions";
 import { getItem } from "./inventory";
 
 console.log("Script started successfully");
 
 let currentPopup: any = undefined;
+let timerPopup: any = undefined;
 let changeDifficultyLevelMessage: any = undefined;
 let startGameMessage: any = undefined;
 let game_tickets: any[] = [];
@@ -20,11 +22,11 @@ let level: number = 1;
 // Waiting for the API to be ready
 WA.onInit()
   .then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
-    console.log('Player name: ', WA.player.name);
-    console.log('Player ID: ', WA.player.id);
-    console.log('Player language: ', WA.player.language);
+    console.log("Scripting API ready");
+    console.log("Player tags: ", WA.player.tags);
+    console.log("Player name: ", WA.player.name);
+    console.log("Player ID: ", WA.player.id);
+    console.log("Player language: ", WA.player.language);
 
     const items = [
       "above/processeur",
@@ -41,36 +43,45 @@ WA.onInit()
       getItem(item);
     });
 
-    game_tickets = getTickets(game_data, level)
+    game_tickets = getTickets(game_data, level);
 
-    WA.room.area.onEnter('changeDifficulty').subscribe(() => {
-        currentPopup = WA.ui.openPopup("showDifficultyPopup", "Niveau de difficulté : " + level, []);
+    WA.room.area.onEnter("changeDifficulty").subscribe(() => {
+      currentPopup = WA.ui.openPopup(
+        "showDifficultyPopup",
+        "Niveau de difficulté : " + level,
+        []
+      );
 
-        changeDifficultyLevelMessage = WA.ui.displayActionMessage({
-            message: "Appuyez sur 'Espace' pour changer le niveau de difficulté",
-            callback: () => {
-                level = (level % game_data.difficulties.length)+1
-                closePopup( currentPopup )
-                currentPopup = WA.ui.openPopup("showDifficultyPopup", "Niveau de difficulté : " + level, []);
-                game_tickets = getTickets(game_data, level)
-            }
-        });
+      changeDifficultyLevelMessage = WA.ui.displayActionMessage({
+        message: "Appuyez sur 'Espace' pour changer le niveau de difficulté",
+        callback: () => {
+          level = (level % game_data.difficulties.length) + 1;
+          closePopup(currentPopup);
+          currentPopup = WA.ui.openPopup(
+            "showDifficultyPopup",
+            "Niveau de difficulté : " + level,
+            []
+          );
+          game_tickets = getTickets(game_data, level);
+        },
+      });
     });
-    WA.room.area.onLeave('changeDifficulty').subscribe(() => {
-        closePopup( currentPopup )
-        changeDifficultyLevelMessage.remove()
+    WA.room.area.onLeave("changeDifficulty").subscribe(() => {
+      closePopup(currentPopup);
+      changeDifficultyLevelMessage.remove();
     });
 
-    WA.room.area.onEnter('startGame').subscribe(() => {
+    WA.room.area.onEnter("startGame").subscribe(() => {
       startGameMessage = WA.ui.displayActionMessage({
         message: "Appuyez sur 'Espace' pour démarrer la partie",
         callback: () => {
+          timerGame(60);
           startGame(game_tickets);
-        }
+        },
       });
     });
-    WA.room.area.onLeave('startGame').subscribe(() => {
-      startGameMessage.remove()
+    WA.room.area.onLeave("startGame").subscribe(() => {
+      startGameMessage.remove();
     });
 
     WA.room.area.onEnter("add_component").subscribe(() => {
@@ -78,51 +89,67 @@ WA.onInit()
     });
 
     WA.room.area.onEnter("processeurPopup").subscribe(() => {
-      showPopup("Processeur", 
-      "Element central de votre ordinateur, responsable de l'execution des programmes et de la gestion des ressources de votre ordinateur.",
-       "processeur");
+      showPopup(
+        "Processeur",
+        "Element central de votre ordinateur, responsable de l'execution des programmes et de la gestion des ressources de votre ordinateur.",
+        "processeur"
+      );
     });
 
     WA.room.area.onEnter("carteMerePopup").subscribe(() => {
-      showPopup("Carte mère", 
-      "Elément central de votre ordinateur, responsable de la communication entre les différents composants de votre ordinateur.",
-       "carteMere");
+      showPopup(
+        "Carte mère",
+        "Elément central de votre ordinateur, responsable de la communication entre les différents composants de votre ordinateur.",
+        "carteMere"
+      );
     });
 
     WA.room.area.onEnter("ramPopup").subscribe(() => {
-      showPopup("Mémoire vive", 
-      "Permet de stocker temporairement les données de vos programmes en cours d'execution.",
-       "ram");
+      showPopup(
+        "Mémoire vive",
+        "Permet de stocker temporairement les données de vos programmes en cours d'execution.",
+        "ram"
+      );
     });
 
     WA.room.area.onEnter("carteGraphiquePopup").subscribe(() => {
-      showPopup("Carte graphique", 
-      "Permet de traiter les informations graphiques de votre ordinateur.",
-       "carteGraphique");
+      showPopup(
+        "Carte graphique",
+        "Permet de traiter les informations graphiques de votre ordinateur.",
+        "carteGraphique"
+      );
     });
 
     WA.room.area.onEnter("ssdPopup").subscribe(() => {
-      showPopup("SSD", 
-      "Permet de stocker de manière permanente vos données et programmes.",
-       "ssd");
+      showPopup(
+        "SSD",
+        "Permet de stocker de manière permanente vos données et programmes.",
+        "ssd"
+      );
     });
 
     WA.room.area.onEnter("disqueDurPopup").subscribe(() => {
-      showPopup("Disque dur", 
-      "Permet de stocker de manière permanente vos données et programmes.",
-       "disqueDur");
+      showPopup(
+        "Disque dur",
+        "Permet de stocker de manière permanente vos données et programmes.",
+        "disqueDur"
+      );
     });
 
     WA.room.area.onEnter("ventiradPopup").subscribe(() => {
-      showPopup("Ventirad", 
-      "Permet de refroidir votre processeur pour éviter la surchauffe.",
-       "ventirad");
+      showPopup(
+        "Ventirad",
+        "Permet de refroidir votre processeur pour éviter la surchauffe.",
+        "ventirad"
+      );
     });
 
     WA.room.area.onEnter("alimentationPopup").subscribe(() => {
-      showPopup("Alimentation", 
-      "Permet d'alimenter votre ordinateur en électricité.",
-       "alimentation");
+      showPopup(
+        "Alimentation",
+        "Permet d'alimenter votre ordinateur en électricité.",
+        "alimentation"
+      );
     });
 
     WA.room.area.onLeave("processeurPopup").subscribe(() => {
@@ -157,78 +184,67 @@ WA.onInit()
       closePopup(currentPopup);
     });
 
-    
-    WA.room.area.onEnter('timer').subscribe(() => {
-        let count = 60;
-        currentPopup = updatePopup(currentPopup, count);
-        const timer = setInterval(() => {
-            count--;
-            currentPopup = updatePopup(currentPopup, count);
-            if (count <= 0) {
-                clearInterval(timer);
-            }
-        }, 1000);
-    });
-
-    WA.room.area.onLeave('timer').subscribe(() => closePopup(currentPopup));
-    
     let podiumWebsite: any;
     let resultWebsite: any;
     let enterCounter = 0;
 
     WA.room.onEnterLayer("podium").subscribe(async () => {
-        enterCounter++;
-        console.log('Entered podium layer', enterCounter, 'times');
-        podiumWebsite = await WA.ui.website.open({
-            url: "./src/podium.html",
-            position: {
-                vertical: "top",
-                horizontal: "middle",
-            },
-            size: {
-                height: "100vh",
-                width: "70vw",
-            },
-            margin: {
-                top: "5vh",
-            },
-            allowApi: true,
-        });
+      enterCounter++;
+      console.log("Entered podium layer", enterCounter, "times");
+      podiumWebsite = await WA.ui.website.open({
+        url: "./src/podium.html",
+        position: {
+          vertical: "top",
+          horizontal: "middle",
+        },
+        size: {
+          height: "100vh",
+          width: "70vw",
+        },
+        margin: {
+          top: "5vh",
+        },
+        allowApi: true,
+      });
     });
 
     WA.room.onLeaveLayer("podium").subscribe(() => {
-        podiumWebsite.close();
+      podiumWebsite.close();
     });
 
     WA.room.onEnterLayer("result").subscribe(async () => {
-        resultWebsite = await WA.ui.website.open({
-            url: "./src/result.html",
-            position: {
-                vertical: "top",
-                horizontal: "middle",
-            },
-            size: {
-                height: "100vh",
-                width: "70vw",
-            },
-            margin: {
-                top: "5vh",
-            },
-            allowApi: true,
-        });
+      resultWebsite = await WA.ui.website.open({
+        url: "./src/result.html",
+        position: {
+          vertical: "top",
+          horizontal: "middle",
+        },
+        size: {
+          height: "100vh",
+          width: "70vw",
+        },
+        margin: {
+          top: "5vh",
+        },
+        allowApi: true,
+      });
     });
 
     WA.room.onLeaveLayer("result").subscribe(() => {
-        resultWebsite.close();
+      resultWebsite.close();
     });
-    
+
     bootstrapExtra()
       .then(() => {
         console.log("Scripting API Extra ready");
       })
       .catch((e) => console.error(e));
 
-    async function showPopup(title: string, description: string, image: string) {
+    async function showPopup(
+      title: string,
+      description: string,
+      image: string
+    ) {
       currentPopup = await WA.ui.website.open({
         url: `./src/componantsPopup.html?title=${title}&description=${description}&image=${image}`,
         position: {
@@ -242,6 +258,31 @@ WA.onInit()
         allowApi: true,
       });
     }
+
+async function timerGame(timer: number) {
+  let count = setInterval(async () => {
+    timer--;
+    if (timerPopup) {
+      await closePopup(timerPopup);
+    }
+    timerPopup = await WA.ui.website.open({
+      url: `./src/timerPopup.html?timer=${timer}`,
+      position: {
+        vertical: "top",
+        horizontal: "middle",
+      },
+      size: {
+        height: "100px",
+        width: "100px",
+      },
+      allowApi: true,
+    });
+    if (timer <= 0) {
+      clearInterval(count);
+      await closePopup(timerPopup);
+    }
+  }, 1000);
+}
   })
   .catch((e) => console.error(e));
 
