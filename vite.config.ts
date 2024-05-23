ea1f35f0198e04bef2baccea69f2695dd88b3064
Coline DARIVON <151158559+Coline-neosoft@users.noreply.github.com>
2024-05-23 11:22:20 +0200

    Add modifs
    ajout de html dans vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 index: "./index.html",
+                betOnAgency : "src/betOnAgency.html",
                 ...getMapsScripts(maps),
             },
         },
